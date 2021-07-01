1c67bfacd5bede478172659be9141c1b83a7f5d6
Abhishek Bhat <abhishekbhat344@gmail.com>
2021-07-01 11:25:30 +0530

    Made Responsive

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let menu = document.getElementById('mobile-item');
+let button = document.getElementById('menu-icon');
+let clos = document.getElementById('close-icon')
+function open_menu(){
+    // menu.style.display ="block";
+    // menu.style.position = "absolute";
+    button.style.display = "none";
+    menu.style.top = "0%";
+    menu.style.transition = "all 0.5s ease";
+}
+function closes(){
+    // menu.style.display = "none"
+    // menu.style.position = "absolute"
+    menu.style.top ="-200%";
+    menu.style.transition = "all 0.5s ease";
+    button.style.display = "block";
+}
+
